a6a2f4e716c938025b2f5a2d9864b4be64462adc
Roni Polak <polak.roni@gmail.com>
2021-12-01 14:50:32 +0200

    Added feature class

---
+++ Face-filter/Feature.py
@@ -0,0 +1,9 @@
+from const_libraries import *
+
+class Feature:
+    def __init__(self, x, y, width, height, weight):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.weight = weight
