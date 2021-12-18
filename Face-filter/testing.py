7932feb8788c9bf159b92df57f3aef3de694269e
Noya Golbandi <noya.golbandi@gmail.com>
2021-12-18 14:40:44 +0200

    changes testing

--- Face-filter/testing.py
+++ Face-filter/testing.py
@@ -1,8 +1,8 @@
 import xmlschema
 
-data_schema = xmlschema.XMLSchema('features.xsd')
-data = data_schema.to_dict('xmlFile')
-stages = data['stages']['_']
+data_schema = xmlschema.XMLSchema('better.xsd')
+data = data_schema.to_dict('better')
+stages = data['haarcascade_frontalface_alt2']['stages']['_']
 feature_counter = 0
 rects_counter = 0
 stages_counter = 0
@@ -10,25 +10,26 @@ for stage in stages:
     print("Stage#" , stages_counter)
     trees = stage['trees']
     for tree in trees['_']:
-        features = tree['_']
-        feature = features['feature']
-        threshold = features['threshold']
-        left_val = features['left_val']
-        right_val = features['right_val']
-        print("Feature #" , feature_counter)
-        #print("rects: ", feature)
-        print("threshold: ",threshold)
-        print("left_val: ",left_val)
-        print("right_val: ",right_val)
-        feature_counter = feature_counter + 1
-        rects = feature['rects']
-        rects_list = rects['_']
-        tilted = feature['tilted']
-        print("Tilted: ", tilted)
-        for mini in rects_list:
-            print("Rect#", rects_counter, mini)
-            rects_counter += 1
-        rects_counter = 0
+        #treenodes = tree
+        for treenode in tree['_']:
+            feature = treenode['feature']
+            threshold = treenode['threshold']
+            #left_val = treenode['left_val']
+            #right_val = treenode['right_val']
+            print("Feature #" , feature_counter)
+            #print("rects: ", feature)
+            print("threshold: ",threshold)
+            #print("left_val: ",left_val)
+            #print("right_val: ",right_val)
+            feature_counter = feature_counter + 1
+            rects = feature['rects']
+            rects_list = rects['_']
+            tilted = feature['tilted']
+            print("Tilted: ", tilted)
+            for mini in rects_list:
+                print("Rect#", rects_counter, mini)
+                rects_counter += 1
+            rects_counter = 0
     feature_counter = 0
     stages_counter += 1
 
