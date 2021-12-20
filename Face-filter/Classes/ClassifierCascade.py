c97f6eeb8757edcff74b0333a2fa1f9fad6111b1
Noya Golbandi <noya.golbandi@gmail.com>
2021-12-20 16:31:16 +0200

    minor changes

--- Face-filter/Classes/ClassifierCascade.py
+++ Face-filter/Classes/ClassifierCascade.py
@@ -1,1 +1,15 @@
+"""
+This is a face detection calssifier based on the haar-cascade algorithm.
 
+Contains the stages.
+
+Contains the main classification function that goes through the features.
+"""
+
+class ClassifierCascade:
+    def __init__(self, stages):
+        #shoudl it get the stages as an argument or should it extract it here?
+        self.stages = stages
+
+    def detect_face(self, image):
+        pass
--- Face-filter/parser.py
+++ Face-filter/parser.py
@@ -1,41 +1,43 @@
 import xmlschema
 
-data_schema = xmlschema.XMLSchema('xml/frontal_face.xsd')
-data = data_schema.to_dict('xml/frontal_face')
-stages = data['haarcascade_frontalface_alt2']['stages']['_']
-feature_counter = 0
-rects_counter = 0
-stages_counter = 0
-for stage in stages:
-    print("Stage#" , stages_counter)
-    trees = stage['trees']
-    for tree in trees['_']:
-        for treenode in tree['_']:
-            print("Feature #", feature_counter)
-            feature = treenode['feature']
-            threshold = treenode['threshold']
-            try:
-                left_val = treenode['left_val']
-                print("left_val: ",left_val)
-            except KeyError:
-                left_node = treenode['left_node']
-                print("left_node: ",left_node)
-            try:
-                right_val = treenode['right_val']
-                print("right_val: ",right_val)
-            except KeyError:
-                right_node = treenode['right_node']
-                print("right_node: ",right_node)
-            print("threshold: ",threshold)
-            feature_counter = feature_counter + 1
-            rects = feature['rects']
-            rects_list = rects['_']
-            tilted = feature['tilted']
-            print("Tilted: ", tilted)
-            for mini in rects_list:
-                print("Rect#", rects_counter, mini)
-                rects_counter += 1
-            rects_counter = 0
+def load_stages():
+    data_schema = xmlschema.XMLSchema('xml/frontal_face.xsd')
+    data = data_schema.to_dict('xml/frontal_face')
+    stages = data['haarcascade_frontalface_alt2']['stages']['_']
+    feature_counter = 0
+    rects_counter = 0
+    stages_counter = 0
+
+    for stage in stages:
+        print("Stage#" , stages_counter)
+        trees = stage['trees']
+        for tree in trees['_']:
+            for treenode in tree['_']:
+                print("Feature #", feature_counter)
+                feature = treenode['feature']
+                threshold = treenode['threshold']
+                try:
+                    left_val = treenode['left_val']
+                    print("left_val: ",left_val)
+                except KeyError:
+                    left_node = treenode['left_node']
+                    print("left_node: ",left_node)
+                try:
+                    right_val = treenode['right_val']
+                    print("right_val: ",right_val)
+                except KeyError:
+                    right_node = treenode['right_node']
+                    print("right_node: ",right_node)
+                print("threshold: ",threshold)
+                feature_counter = feature_counter + 1
+                rects = feature['rects']
+                rects_list = rects['_']
+                tilted = feature['tilted']
+                print("Tilted: ", tilted)
+                for mini in rects_list:
+                    print("Rect#", rects_counter, mini)
+                    rects_counter += 1
+                rects_counter = 0
     feature_counter = 0
     stages_counter += 1
 
