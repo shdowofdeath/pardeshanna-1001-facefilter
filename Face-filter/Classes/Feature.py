51604bef389c6a39d53034f8a5ca78c1dd2802ea
Noya Golbandi <noya.golbandi@gmail.com>
2022-01-10 21:18:04 +0200

    added calc_rects_diff

--- Face-filter/Classes/Feature.py
+++ Face-filter/Classes/Feature.py
@@ -1,3 +1,4 @@
+import Rect
 """
 This is a class of a haar feature.
 
@@ -18,13 +19,88 @@ class Feature:
         #get value of rect1
         #get value of rect2
         #calculate the difference between them
-        #return if the difference is greater than 1
+        #return if the difference is closer to 1 (1/threshold?)
         pass
 
-    def calc_rects_diff(self):
+    def calc_rects_diff(self, mini_grid):
         #check the number of rects there are:
         #if there are two => send both to "is_diff" and return if there is a difference
         #if there are three => send two sets of rects to "is_diff" and return if there is a difference in BOTH
-        #if there are four => send four sets of rects "is_diff" and return if there is a difference in ALL
+        #if there are three => send four sets of rects "is_diff" and return if there is a difference in ALL
         #we are not checking tilted rects or "one in one" rects for now
+
+        if(not self.is_tilted):#handling not tilted
+            if self.rects.count == 2:
+                if self.rects[0].x == self.rects[1].x: #lays
+                    #y && height
+                    if((self.rects[1].y - self.rects[0].y) == (self.rects[0].height - self.rects[1].height)):
+                        #2 rects
+                        if(self.is_diff(mini_grid,self.rects[0], self.rects[1])):
+                            #success (return true)
+                            pass
+                        else:
+                            #fail
+                            pass
+                    elif((self.rects[1].y - self.rects[0].y) * 2 == (self.rects[0].height - self.rects[1].height)):
+                        #3 rects
+                        middle = self.rects[1]
+                        above = Rect(self.rects[0].x, self.rects[0].y, self.rects[0].width, self.rects[0].height / 3, 0)
+                        below = Rect(self.rects[0].x, self.rects[1].y + (self.rects[0].height / 3), self.rects[0].width, self.rects[0].height / 3, 0)
+                        if(self.is_diff(mini_grid,above, middle) and self.is_diff(mini_grid,below, middle)):
+                            #success
+                            pass
+                        else:
+                            #fail
+                            pass
+                elif self.rects[0].y == self.rects[1].y: #stands
+                    #x && width
+                    if ((self.rects[1].x - self.rects[0].x) == (self.rects[0].width - self.rects[1].width)):
+                        # 2 rects
+                        if (self.is_diff(mini_grid, self.rects[0], self.rects[1])):
+                            # success (return true)
+                            pass
+                        else:
+                            # fail
+                            pass
+                    elif ((self.rects[1].x - self.rects[0].x) * 2 == (self.rects[0].width - self.rects[1].width)):
+                        # 3 rects
+                        middle = self.rects[1]
+                        left = Rect(self.rects[0].x, self.rects[0].y, self.rects[0].width / 3, self.rects[0].height, 0)
+                        right = Rect(self.rects[1].x + (self.rects[0].width / 3), self.rects[0].y, self.rects[0].width / 3,
+                                     self.rects[0].height, 0)
+                        if (self.is_diff(mini_grid, left, middle) and self.is_diff(mini_grid, right, middle)):
+                            # success
+                            pass
+                        else:
+                            # fail
+                            pass
+                else:
+                    #one in one
+                    pass
+            elif self.rects.count() == 3:#there are 4 rects
+                if(self.rects[0].x == self.rects[1].x and self.rects[0].y == self.rects[1].y): #same starting point
+                    top_left = self.rects[1]
+                    top_right = Rect(self.rects[0].x + (self.rects[0].width / 2), self.rects[0].y, self.rects[0].width / 2, self.rects[0].height / 2, 0)
+                    bottom_left = Rect(self.rects[0].x, self.rects[0].y + (self.rects[0].height / 2), self.rects[0].width / 2, self.rects[0].height / 2, 0)
+                    bottom_right = self.rects[2]
+
+                    if self.is_diff(mini_grid, top_left, top_right) and self.is_diff(mini_grid, top_left, bottom_left) and self.is_diff(mini_grid, top_right, bottom_right) and self.is_diff(mini_grid, bottom_left, bottom_right):
+                        #success
+                        pass
+                    else:
+                        #fail
+                        pass
+                else:
+                    top_left = Rect(self.rects[0].x, self.rects[0].y, self.rects[0].width / 2, self.rects[0].height / 2, 0)
+                    top_right = self.rects[1]
+                    bottom_left = self.rects[2]
+                    bottom_right = Rect(self.rects[0].x + (self.rects[0].width / 2), self.rects[0].y + (self.rects[0].height / 2), self.rects[0].width / 2, self.rects[0].height / 2, 0)
+
+                    if self.is_diff(mini_grid, top_left, top_right) and self.is_diff(mini_grid, top_left, bottom_left) and self.is_diff(mini_grid, top_right, bottom_right) and self.is_diff(mini_grid, bottom_left, bottom_right):
+                        #success
+                        pass
+                    else:
+                        #fail
+                        pass
+
         pass
--- Face-filter/Classes/Rect.py
+++ Face-filter/Classes/Rect.py
@@ -16,8 +16,8 @@ class Rect:
         #use integral image to calc the value
         #returns the value of the rect
 
-        w = mini_grid[self.x - 1][self.y - 1] if (self.x > 0 and self.y>0) else 0
+        w = mini_grid[self.x - 1][self.y - 1] if (self.x > 0 and self.y > 0) else 0
         x = mini_grid[self.x][self.y - 1] if (self.y > 0) else 0
         y = mini_grid[self.x - 1][self.y] if (self.x > 0) else 0
-        z = mini_grid[self.x + self.width - 1][self.y +self.height - 1]
+        z = mini_grid[self.x + self.width - 1][self.y + self.height - 1]
         return z - x - y + w
